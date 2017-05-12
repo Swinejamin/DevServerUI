5cd76197f7472173933708ab189ba5bf4a29c41c
Benjamin Swineford <benjamin@swineford.me>
2017-05-12 13:19:56 -0500

    took out github call for now

--- src/server/directory.php
+++ src/server/directory.php
@@ -11,16 +11,46 @@ foreach ($dirs as $dir => $ref) {
 
     $innerHTML = '';
     $dom = new DOMDocument();
-    $target = "$cwd . \"/\" . $ref";
+    $target = realpath($cwd . DIRECTORY_SEPARATOR . $ref);
     $indexPath = realpath($cwd . DIRECTORY_SEPARATOR . $ref . DIRECTORY_SEPARATOR . "index.html");
-    $file = file_get_contents($cwd . "/" . $ref . "/index.html");
+    $file = file_get_contents($indexPath);
     $dom->loadHTML($file);
     $title = $dom->getElementsByTagName('title')[0];
     $innerHTML .= html_entity_decode(strip_tags($title->ownerDocument->saveHTML($title)));
     $dir['title'] = $innerHTML;
+    $email = "bmswine@gmail.com";
+    $keyhook = Tqyglfjs30rBDLmDD3pH;
     $stat = stat($indexPath);
-    $created = json_encode($stat["ctime"]);
-    $modified = json_encode($stat["mtime"]);
+
+
+
+    // create curl resource
+    $ch = curl_init();
+    $repoUrl = "https://api.github.com/repos/swinejamin/$ref";
+    // set url
+    curl_setopt($ch, CURLOPT_URL, $repoUrl);
+    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, "Accept: application/vnd.github.v3+json");
+    curl_setopt($ch, CURLOPT_USERAGENT, "https://github.com/Swinejamin");
+    curl_setopt($ch, CURLOPT_USERPWD, "$email:$keyhook");
+    $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    //return the transfer as a string
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    // $output contains the output string
+//    $response = curl_exec($ch);
+//    $repo = json_decode($response);
+
+//    $created = $repo["created_at"];
+//    $modified = $repo["modified_at"];
+//    $created = $response['created_at'];
+//    $created = $repo->created_at;
+//    $modified = $repo->modified_at;
+    $created = json_encode($stat['ctime']);
+    $modified = json_encode($stat['mtime']);
+
+    // close curl resource to free up system resources
+    curl_close($ch);
 
     $newArr = array('directory' => $ref, "title" => $innerHTML, "stat" => $stat, "created" => $created, "modified" => $modified);
     array_push($projects, $newArr);
